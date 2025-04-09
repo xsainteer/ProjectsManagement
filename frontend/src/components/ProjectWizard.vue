6b1da59d8e9961447f266b942f05748d8118e718
xsainteer <makmatov123@gmail.com>
2025-04-09 15:48:58 +0600

    added new features and defined wizard more, API call and local storage saves to be done

--- frontend/src/components/ProjectWizard.vue
+++ frontend/src/components/ProjectWizard.vue
@@ -16,27 +16,27 @@
       </el-form-item>
       <el-form-item label="Приоритет">
         <el-select v-model="form.priority" placeholder="Выберите приоритет">
-          <el-option label="Высокий" value="high" />
-          <el-option label="Средний" value="medium" />
-          <el-option label="Низкий" value="low" />
+          <el-option label="Высокий" value="3" />
+          <el-option label="Средний" value="2" />
+          <el-option label="Низкий" value="1" />
         </el-select>
       </el-form-item>
     </el-form>
   </div>
 
   <div v-if="activeStep === 1">
-    <el-form-item label="Компания-заказчик">
+    <el-form-item label="Client Company">
       <el-input v-model="form.clientCompany" />
     </el-form-item>
 
-    <el-form-item label="Добавить сотрудника в заказчика">
-      <el-input v-model="newClientEmployee.name" placeholder="Имя" style="margin-bottom: 6px" />
-      <el-input v-model="newClientEmployee.speciality" placeholder="Должность" style="margin-bottom: 6px" />
-      <el-button type="primary" @click="addEmployee('client')">Добавить</el-button>
+    <el-form-item label="Add an employee">
+      <el-input v-model="newClientEmployee.name" placeholder="Name" style="margin-bottom: 6px" />
+      <el-input v-model="newClientEmployee.speciality" placeholder="Speciality" style="margin-bottom: 6px" />
+      <el-button type="primary" @click="addEmployee('client')">Add</el-button>
     </el-form-item>
 
     <el-table :data="form.clientCompanyStaff" style="width: 100%; margin-bottom: 20px">
-      <el-table-column prop="name" label="Имя">
+      <el-table-column prop="name" label="Name">
         <template #default="{ row }">
           <template v-if="row.isEditing">
             <el-input v-model="row.editName" />
@@ -45,7 +45,7 @@
         </template>
       </el-table-column>
 
-      <el-table-column prop="speciality" label="Должность">
+      <el-table-column prop="speciality" label="Speciality">
         <template #default="{ row }">
           <template v-if="row.isEditing">
             <el-input v-model="row.editSpeciality" />
@@ -54,94 +54,193 @@
         </template>
       </el-table-column>
 
-      <el-table-column label="Действия" width="160">
+      <el-table-column label="Actions" width="160">
         <template #default="{ row }">
           <el-button
               v-if="row.isEditing"
               size="small"
               type="success"
               @click="saveEmployee(row)"
-          >Сохранить</el-button>
+          >Save</el-button>
           <el-button
               v-else
               size="small"
               @click="editEmployee(row)"
-          >Изменить</el-button>
-          <el-button size="small" type="danger" @click="deleteEmployee('client', row.id)">Удалить</el-button>
+          >Change</el-button>
+          <el-button size="small" type="danger" @click="deleteEmployee('client', row.id)">Delete</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
 
-
   <div v-if="activeStep === 2">
-    <el-form-item label="Руководитель проекта">
-      <el-select
-          v-model="form.manager"
-          filterable
-          remote
-          reserve-keyword
-          placeholder="Поиск по имени"
-          :remote-method="searchEmployees('contractor')"
-          :loading="loading"
-      >
-        <el-option
-            v-for="employee in employees"
-            :key="employee.id"
-            :label="employee.name"
-            :value="employee.id"
-        />
-      </el-select>
+    <el-form-item label="Contractor Company">
+      <el-input v-model="form.contractorCompany" />
     </el-form-item>
+
+    <el-form-item label="Add an employee">
+      <el-input v-model="newContractorEmployee.name" placeholder="Name" style="margin-bottom: 6px" />
+      <el-input v-model="newContractorEmployee.speciality" placeholder="Speciality" style="margin-bottom: 6px" />
+      <el-button type="primary" @click="addEmployee('contractor')">Add</el-button>
+    </el-form-item>
+
+    <el-table :data="form.contractorCompanyStaff" style="width: 100%; margin-bottom: 20px">
+      <el-table-column prop="name" label="Name">
+        <template #default="{ row }">
+          <template v-if="row.isEditing">
+            <el-input v-model="row.editName" />
+          </template>
+          <template v-else>{{ row.name }}</template>
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="speciality" label="Speciality">
+        <template #default="{ row }">
+          <template v-if="row.isEditing">
+            <el-input v-model="row.editSpeciality" />
+          </template>
+          <template v-else>{{ row.speciality }}</template>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="Actions" width="160">
+        <template #default="{ row }">
+          <el-button
+              v-if="row.isEditing"
+              size="small"
+              type="success"
+              @click="saveEmployee(row)"
+          >Save</el-button>
+          <el-button
+              v-else
+              size="small"
+              @click="editEmployee(row)"
+          >Change</el-button>
+          <el-button size="small" type="danger" @click="deleteEmployee('contractor', row.id)">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 
   <div v-if="activeStep === 3">
-    <el-form-item label="Исполнители проекта">
+    <el-form-item label="Manager">
       <el-select
-          v-model="form.performers"
+          v-model="form.managerId"
           filterable
-          multiple
           remote
-          placeholder="Поиск сотрудников"
-          :remote-method="searchEmployees"
+          reserve-keyword
+          placeholder="Search by name"
+          :remote-method="(query) => searchEmployees('contractor', query)"
           :loading="loading"
       >
         <el-option
-            v-for="employee in employees"
+            v-for="employee in filteredEmployees"
             :key="employee.id"
             :label="employee.name"
             :value="employee.id"
         />
       </el-select>
+      <div v-if="form.managerId">
+        <p>Chosen manager:
+          {{ form.contractorCompanyStaff.find(e => e.id === form.managerId)?.name }}
+        </p>
+      </div>
     </el-form-item>
   </div>
 
   <div v-if="activeStep === 4">
-    <el-upload
-        class="upload-demo"
-        drag
-        action="https://your-upload-endpoint.com"
-        multiple
+    <el-form-item label="Performers">
+      <el-select
+          v-model="form.performersId"
+          filterable
+          multiple
+          remote
+          placeholder="Search by name"
+          :remote-method="(query) => searchEmployees('contractor', query)"
+          :loading="loading"
+      >
+        <el-option
+            v-for="employee in filteredEmployees"
+            :key="employee.id"
+            :label="employee.name"
+            :value="employee.id"
+        />
+      </el-select>
+    </el-form-item>
+    
+    <el-table :data="form.performersId">
+      <el-table-column prop="name" label="Name">
+        <template #default="{ row }">
+          {{form.contractorCompanyStaff.find(e => e.id === row)?.name}}
+        </template>  
+      </el-table-column>
+
+      <el-table-column prop="speciality" label="Speciality">
+        <template #default="{ row }">
+          {{form.contractorCompanyStaff.find(e => e.id === row)?.speciality}}
+        </template>
+      </el-table-column>
+
+      <el-table-column label="Actions" width="160">
+        <template #default="{ row }">
+          <el-button size="small" type="danger" 
+                     @click="
+                       form.performersId = form.performersId.filter(id => id !== row)
+                     ">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+
+  <div v-if="activeStep === 5">
+    <div
+        class="upload-container"
+        @dragover.prevent="handleDragOver"
+        @dragleave.prevent="handleDragLeave"
+        @drop.prevent="handleDrop"
     >
-      <i class="el-icon-upload" />
-      <div class="el-upload__text">Перетащите файлы сюда или <em>нажмите для выбора</em></div>
-    </el-upload>
+      <div
+          class="upload-box"
+          :class="{ 'is-dragging': isDragging }"
+          @click="triggerFileInput"
+      >
+        <input
+            type="file"
+            multiple
+            ref="fileInput"
+            class="file-input"
+            @change="handleFileChange"
+        />
+        <i class="el-icon-upload" />
+        <div class="upload-text">
+          Drag your files here or <em>click to choose</em>
+        </div>
+      </div>
+      <div v-if="fileList.length > 0">
+        <h3>Uploaded Files:</h3>
+        <ul>
+          <li v-for="(file, index) in fileList" :key="file.name + index">
+            {{ file.name }} - {{ file.size }} bytes
+            <el-button size="small" type="danger" @click="removeFile(index)">Remove</el-button>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 
   <div class="mt-4 flex justify-between">
-    <el-button @click="prevStep" :disabled="activeStep === 0">Назад</el-button>
+    <el-button @click="prevStep" :disabled="activeStep === 0">Back</el-button>
     <el-button type="primary" @click="nextStep">
-      {{ activeStep === steps.length - 1 ? 'Готово' : 'Далее' }}
+      {{ activeStep === steps.length - 1 ? 'Finish' : 'Next' }}
     </el-button>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import axios from 'axios'
 
 const activeStep = ref(0)
-const steps = ['Инфо о проекте', 'Компании', 'Руководитель', 'Исполнители', 'Документы']
+const steps = ['Project Information', 'Client Company', 'Contractor Company', 'Supervisor selection', 'Performers', 'Documents']
 
 const form = ref({
   projectName: '',
@@ -150,24 +249,55 @@ const form = ref({
   priority: '',
   clientCompany: '',
   contractorCompany: '',
-  manager: '',
-  performers: [],
+  managerId: '',
+  performersId: [],
   clientCompanyStaff: [],
   contractorCompanyStaff: []
 })
 
+const fileList = ref([])
+const isDragging = ref(false)
+const fileInput = ref(null)
 
-const employees = ref([])
+const removeFile = (index) => {
+  fileList.value.splice(index, 1)
+}
+
+function handleFileChange(event) {
+  const newFiles = Array.from(event.target.files)
+  fileList.value = [...fileList.value, ...newFiles]
+  event.target.value = ''
+}
+
+function handleDragOver(event) {
+  isDragging.value = true;
+}
+
+function handleDragLeave() {
+  isDragging.value = false;
+}
+
+function handleDrop(event) {
+  const files = event.dataTransfer.files;
+  fileList.value = Array.from(files);
+  isDragging.value = false;
+}
+
+function triggerFileInput() {
+  fileInput.value?.click()
+}
+
 const loading = ref(false)
 
 const newClientEmployee = ref({ name: '', speciality: '' })
 const newContractorEmployee = ref({ name: '', speciality: '' })
 
-let clientEmployeeCounter = 0
-let contractorEmployeeCounter = 0
+let clientEmployeeCounter = 1
+let contractorEmployeeCounter = 1 
 
 function addEmployee(target) {
   const employee = {
+    id: target === 'client' ? clientEmployeeCounter : contractorEmployeeCounter,
     name: target === 'client' ? newClientEmployee.value.name : newContractorEmployee.value.name,
     speciality: target === 'client' ? newClientEmployee.value.speciality : newContractorEmployee.value.speciality,
     isEditing: false,
@@ -195,11 +325,33 @@ function saveEmployee(emp) {
   emp.isEditing = false
 }
 
-function searchEmployees(target) {
-  if (target === 'client') {
-    return form.value.clientCompanyStaff
-  } else if (target === 'contractor') {
-    return form.value.contractorCompanyStaff
+const filteredEmployees = ref([])
+
+function searchEmployees(target, query) {
+  loading.value = true
+
+  if (!query || query.trim() === '') {
+    filteredEmployees.value = [];
+    loading.value = false;
+    return;
+  }
+  
+  let list = target === 'client'
+  ? form.value.clientCompanyStaff
+  : form.value.contractorCompanyStaff
+  
+  filteredEmployees.value = list.filter(e =>
+  e.name.toLowerCase().includes(query.toLowerCase()))
+  
+  loading.value = false
+}
+
+function deleteEmployee(target, id) {
+  if(target === 'client') {
+    form.value.clientCompanyStaff = form.value.clientCompanyStaff.filter(e => e.id !== id)
+  }
+  if(target === 'contractor') {
+    form.value.contractorCompanyStaff = form.value.contractorCompanyStaff.filter(e => e.id !== id)
   }
 }
 
@@ -218,5 +370,59 @@ const prevStep = () => {
   padding: 40px;
   text-align: center;
   background-color: #f9f9f9;
+}
+.upload-container {
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
+  text-align: center;
+  padding: 20px;
+}
+
+.upload-box {
+  width: 100%;
+  border: 2px dashed #cccccc;
+  padding: 40px;
+  cursor: pointer;
+  transition: border-color 0.3s, background-color 0.3s;
+}
+
+.upload-box.is-dragging {
+  border-color: #4caf50;
+  background-color: #e8f5e9;
+}
+
+.upload-box:hover {
+  border-color: #4caf50;
+}
+
+.upload-box .el-icon-upload {
+  font-size: 40px;
+  color: #4caf50;
+  margin-bottom: 10px;
+}
+
+.upload-text {
+  font-size: 16px;
+  color: #888;
+}
+
+.upload-text em {
+  color: #4caf50;
+}
+
+.file-input {
+  display: none;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+ul li {
+  font-size: 14px;
+  color: #333;
+  padding: 5px 0;
 }
 </style>
